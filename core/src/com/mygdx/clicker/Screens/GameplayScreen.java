7406eefbe78d18e9d0d6978bc9abff5c66a7033f
Mateusz <mateuszd2411@gmail.com>
2020-04-25 11:14:07 +0200

    Displaying Player Image

--- core/src/com/mygdx/clicker/Screens/GameplayScreen.java
+++ core/src/com/mygdx/clicker/Screens/GameplayScreen.java
@@ -1,8 +1,11 @@
 package com.mygdx.clicker.Screens;
 
 import com.mygdx.clicker.ClickerGame;
+import com.mygdx.clicker.Entities.Player;
 
 public class GameplayScreen extends AbstractScreen{
+
+    private Player player;
 
     public GameplayScreen(ClickerGame game) {
         super(game);
@@ -10,6 +13,26 @@ public class GameplayScreen extends AbstractScreen{
     }
 
     private void init() {
+        initPlayer();
+    }
 
+    private void initPlayer() {
+        player = new Player();
+        stage.addActor(player);
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        update();
+
+        spriteBatch.begin();
+        stage.draw();
+        spriteBatch.end();
+    }
+
+    private void update() {
+        stage.act();
     }
 }
+
