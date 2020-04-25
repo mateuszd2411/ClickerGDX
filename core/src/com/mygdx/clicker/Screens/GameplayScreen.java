42cc8d1693d61db82f5011272ddfb66eae524d24
Mateusz <mateuszd2411@gmail.com>
2020-04-25 11:43:35 +0200

    add player button and click listener

--- core/src/com/mygdx/clicker/Screens/GameplayScreen.java
+++ core/src/com/mygdx/clicker/Screens/GameplayScreen.java
@@ -1,11 +1,15 @@
 package com.mygdx.clicker.Screens;
 
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.mygdx.clicker.ClickerGame;
 import com.mygdx.clicker.Entities.Player;
 
 public class GameplayScreen extends AbstractScreen{
 
     private Player player;
+    private Button playerButton;
 
     public GameplayScreen(ClickerGame game) {
         super(game);
@@ -13,6 +17,28 @@ public class GameplayScreen extends AbstractScreen{
 
     protected void init() {
         initPlayer();
+        initPlayerButton();
+    }
+
+    private void initPlayerButton() {
+        playerButton = new Button(new Button.ButtonStyle());
+        playerButton.setWidth(460);
+        playerButton.setHeight(360);
+        playerButton.setX(10);
+        playerButton.setY(170);
+        playerButton.setDebug(true);
+
+        stage.addActor(playerButton);
+
+        playerButton.addListener(new ClickListener(){
+
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y,
+                                     int pointer, int button) {
+                System.out.println("click");
+                return super.touchDown(event, x, y, pointer, button);
+            }
+        });
     }
 
     private void initPlayer() {
