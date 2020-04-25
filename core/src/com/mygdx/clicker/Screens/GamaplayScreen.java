8b06e874e78f1f696b98c1a12747028a8e869654
Mateusz <mateuszd2411@gmail.com>
2020-04-25 10:43:17 +0200

    I added dummy game play Screen

---
+++ core/src/com/mygdx/clicker/Screens/GamaplayScreen.java
@@ -0,0 +1,15 @@
+package com.mygdx.clicker.Screens;
+
+import com.mygdx.clicker.ClickerGame;
+
+public class GamaplayScreen extends AbstractScreen{
+
+    public GamaplayScreen(ClickerGame game) {
+        super(game);
+        init();
+    }
+
+    private void init() {
+
+    }
+}
