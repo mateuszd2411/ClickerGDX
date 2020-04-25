d4a0d7ca0bb05c8e39ab07ddd1ee5bd1add3c78b
Mateusz <mateuszd2411@gmail.com>
2020-04-25 14:35:37 +0200

    I added saving player score

--- core/src/com/mygdx/clicker/ClickerGame.java
+++ core/src/com/mygdx/clicker/ClickerGame.java
@@ -2,6 +2,7 @@ package com.mygdx.clicker;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -9,12 +10,17 @@ import com.mygdx.clicker.Screens.SplashScreen;
 
 public class ClickerGame extends Game {
 
+	public final  static  String GAME_PREFS = "com.mygdx.clicker.prefs";
+	public final  static  String GAME_SCORE = "com.mygdx.clicker.prefs.score";
+
 	public static  String GAME_NAME = "Clicker";
 
 	public static int WIDTH = 480;
 	public static int HEIGHT = 700;
 
 	private boolean paused;
+
+	private Preferences prefs;
 
 	private int points;
 
@@ -24,11 +30,23 @@ public class ClickerGame extends Game {
 	
 	@Override
 	public void create () {
+		init();
 		this.setScreen(new SplashScreen(this));
+	}
+
+	private void init() {
+		prefs = Gdx.app.getPreferences(GAME_PREFS);
+		loadScore();
+	}
+
+	private void loadScore() {
+		points = prefs.getInteger(GAME_SCORE);
 	}
 
 	public void addPoint(){
 		points++;
+		prefs.putInteger(GAME_SCORE,points);
+		prefs.flush();
 	}
 
 	/**
