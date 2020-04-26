0f83ac720739e12a697e1b9b24667ed23388ce75
Mateusz <mateuszd2411@gmail.com>
2020-04-26 13:37:47 +0200

    added passive income filed to ScoreService

--- core/src/com/mygdx/clicker/Service/ScoreService.java
+++ core/src/com/mygdx/clicker/Service/ScoreService.java
@@ -7,10 +7,12 @@ public class ScoreService {
 
     public final  static  String GAME_PREFS = "com.mygdx.clicker.prefs";
     public final  static  String GAME_SCORE = "com.mygdx.clicker.prefs.score";
+    public final  static  String GAME_PASSIVE_INCOME = "com.mygdx.clicker.prefs.passiveincome";
 
     private Preferences prefs;
 
     private int points;
+    private int passiveIncome;
 
     public ScoreService(){
         init();
@@ -19,35 +21,43 @@ public class ScoreService {
     private void init() {
         prefs = Gdx.app.getPreferences(GAME_PREFS);
         loadScore();
+        loadPassiveIncome();
     }
 
     private void loadScore() {
         points = prefs.getInteger(GAME_SCORE);
     }
 
+    private void loadPassiveIncome(){
+        passiveIncome = prefs.getInteger(GAME_PASSIVE_INCOME);
+    }
+
     public void addPoints(int pointsToAdd){
         points += pointsToAdd;
-        updateSavedScoreInPrefs();
+        updateSavedScoreAndPassiceIncomeInPrefs();
     }
 
     public void addPoint(){
         points++;
-        updateSavedScoreInPrefs();
+        updateSavedScoreAndPassiceIncomeInPrefs();
     }
 
     public void resetGameScore() {
         points = 0;
-        updateSavedScoreInPrefs();
+        passiveIncome = 0;
+        updateSavedScoreAndPassiceIncomeInPrefs();
     }
 
-    private void updateSavedScoreInPrefs() {
+    private void updateSavedScoreAndPassiceIncomeInPrefs() {
         prefs.putInteger(GAME_SCORE, points);
+        prefs.putInteger(GAME_PASSIVE_INCOME,passiveIncome);
         prefs.flush();
     }
 
     public void addPassiveIncome() {
-        //todo implement
-        System.out.println("passive income type");
+       passiveIncome++;
+       updateSavedScoreAndPassiceIncomeInPrefs();
+        System.out.println("passive Income " + passiveIncome);
     }
 
     public int getPoints() {
